bc18d254aa827bab273156271701973676837b2f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-11-25 20:17:30 +0000

    Deploy sumanbarick/skillseval to github.com/sumanbarick/skillseval.git:gh-pages

---
+++ scripts/properties.js
@@ -0,0 +1,18 @@
+(function(){
+	angular._9 = angular._9 || {
+        "hostType": "web",
+        "webRoot": "",
+		"appRoot": "https://knexaa.bitbucket.io",
+		"dataRoot": "data",
+        "xlib": {
+            index: "raw:library",
+            coding: "raw:library/coding"
+        },
+        
+        "config": {
+            "homeConfig": "configs/homeConfig.json"
+        },
+        
+        "uniKey": "7744863-14135635-13322038-2015080-16136753-19833163-12514593-17757518-4579128-20503651-7431460-6726711-16517190-5287019-14555213-6265901-17583703-4939299-14636857-8574514-19331705-23233876-11785833-4673144-13136164-10791488-16075864-24241465-11007347-23276122"
+	};
+})();
